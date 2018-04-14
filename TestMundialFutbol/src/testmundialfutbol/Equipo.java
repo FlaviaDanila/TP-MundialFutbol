b8ee23d0fa28a3b8cba1871eeb3ab257e85792d7
Flavia Danila Yakimiuk <yaki3011@gmail.com>
2018-04-14 17:56:55 -0300

    difGoles

--- TestMundialFutbol/src/testmundialfutbol/Equipo.java
+++ TestMundialFutbol/src/testmundialfutbol/Equipo.java
@@ -14,37 +14,70 @@ public class Equipo {
     private Resultado resultado;
     private Equipo local;
     private Equipo visitante;
-    
-    public Equipo (Resultado resultado, int golesLocal, int golesVisitante) {
+    private int golesQueHice;
+    private int golesQueMeHicieron;
+    private int diferenciaDeGoles;
+
+    public Equipo(Resultado resultado, int golesLocal, int golesVisitante) {
         this.resultado.setResultado(golesLocal, golesVisitante);
     }
 
-    public ArrayList<Partido> getPartidosJugados() {
-        return this.getPartidosJugados();
-    }
-
-    public void setPartidosJugados(ArrayList<Partido> PartidosJugados) {
-        this.partidosJugados = partidosJugados;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getNombre() {
         return this.nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setPartidosJugados(ArrayList<Partido> partidosJugados) {
+        this.partidosJugados = partidosJugados;
     }
-    
+
+    public ArrayList<Partido> getPartidosJugados() {
+        return this.getPartidosJugados();
+    }
+
     public Resultado getResultado() {
         return this.resultado;
     }
-    
+
     public Equipo getLocal() {
         return this.local;
     }
-    
+
     public Equipo getVisitante() {
         return this.visitante;
     }
-    
+
+    public int getGolesQueHice() {
+        return golesQueHice;
+    }
+
+    public void setGolesQueHice(int golesQHice) {
+        this.golesQueHice = golesQueHice;
+    }
+
+    public int getGolesQueMeHicieron() {
+        return golesQueMeHicieron;
+    }
+
+    public void setGolesQueMeHicieron(int golesQMeHicieron) {
+        this.golesQueMeHicieron = golesQueMeHicieron;
+    }
+
+    public int getDiferenciaDeGoles() {
+        if (this.golesQueHice > this.golesQueMeHicieron) {
+            diferenciaDeGoles = this.golesQueHice - this.golesQueMeHicieron;
+        } 
+        else {
+            diferenciaDeGoles = this.golesQueMeHicieron - this.golesQueHice;
+        }
+        return this.diferenciaDeGoles;
+    }
+
+    public void setDiferenciaDeGoles(int diferenciaDeGoles) {
+        this.diferenciaDeGoles = diferenciaDeGoles;
+    }
+
 }
